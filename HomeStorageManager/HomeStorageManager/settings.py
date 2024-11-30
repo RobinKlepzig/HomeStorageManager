cbdb591a190d98b5ecabfd86b2d3a75d7ae9d96f
r-klepzig <webmaster@r-klepzig.de>
2024-11-30 10:41:53 +0100

    Disable Debug

--- HomeStorageManager/HomeStorageManager/settings.py
+++ HomeStorageManager/HomeStorageManager/settings.py
@@ -17,7 +17,7 @@ except KeyError:
     HOSTNAMEANDPORT = '127.0.0.1:8000'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
