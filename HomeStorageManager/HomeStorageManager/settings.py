46436d442aad5516e1ee2b7f50ace5bfe6dc66a8
r-klepzig <webmaster@r-klepzig.de>
2025-01-16 12:23:48 +0100

    Change settings.py

--- HomeStorageManager/HomeStorageManager/settings.py
+++ HomeStorageManager/HomeStorageManager/settings.py
@@ -19,7 +19,7 @@ except KeyError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1','http://localhost']
 
 # Application definition
 
