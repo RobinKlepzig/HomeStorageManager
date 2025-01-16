5e14108b8497b27b54ef8d9fc1e8ca28b68b52dc
r-klepzig <webmaster@r-klepzig.de>
2025-01-16 12:47:53 +0100

    remove fix

--- HomeStorageManager/HomeStorageManager/settings.py
+++ HomeStorageManager/HomeStorageManager/settings.py
@@ -19,7 +19,6 @@ except KeyError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000','http://localhost:8000']
 
 # Application definition
 
